c86b055843d8863ca6d8f15e3c26df8aace688cd
Charoline Septa <charoline.septa@si.ukdw.ac.id>
2017-04-11 19:32:58 +0700

    Test Delete

--- user_admin/delete_beasiswa.php
+++ user_admin/delete_beasiswa.php
@@ -3,4 +3,5 @@ $kode_beasiswa = $_GET['kode_bsw'];
 $q0 = "DELETE from beasiswa where kd_bsw='$kode_beasiswa'";
 $squery = mysqli_query($koneksi,$q0);
 $row1 = mysqli_fetch_array($squery);
+//test
 ?>
