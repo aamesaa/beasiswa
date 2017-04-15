58afe99bc998ba42d58f13f0b72c83b958f90972
Charoline Septa <charoline.septa@si.ukdw.ac.id>
2017-04-15 18:15:36 +0700

    Detail Pendaftar

--- user_admin/detail_pendaftar.php
+++ user_admin/detail_pendaftar.php
@@ -82,25 +82,23 @@ $rowBsw = mysqli_fetch_array($bswExc);
             <a href="index.php">Back to Home</a>
             <h2 class="text-center">Detail Pendaftar</h2>
             <br/>
-            <div class="alert alert-info">
-                <table>
-                    <tr>
-                        <td>Nama Beasiswa &nbsp; </td>
-                        <td>:&nbsp;</td>
-                        <td><?php echo $rowBsw[0];?> </td>
-                    </tr>
-                    <tr>
-                        <td>Semester</td>
-                        <td>:&nbsp;</td>
-                        <td><?php echo $rowBsw[1];?></td>
-                    </tr>
-                    <tr>
-                        <td> Thn Ajaran </td>
-                        <td>:&nbsp;</td>
-                        <td> <?php echo $rowBsw[2];?><br/> </td>
-                    </tr>
-                </table>
-            </div>
+            <table>
+                <tr>
+                    <td>Nama Beasiswa &nbsp; </td>
+                    <td>:&nbsp;</td>
+                    <td><?php echo $rowBsw[0];?> </td>
+                </tr>
+                <tr>
+                    <td>Semester</td>
+                    <td>:&nbsp;</td>
+                    <td><?php echo $rowBsw[1];?></td>
+                </tr>
+                <tr>
+                    <td> Thn Ajaran </td>
+                    <td>:&nbsp;</td>
+                    <td> <?php echo $rowBsw[2];?><br/> </td>
+                </tr>
+            </table>
             <br/>
             <table class="table table-striped">
                 <tr>
@@ -155,15 +153,15 @@ $rowBsw = mysqli_fetch_array($bswExc);
                         if(!$hasil0[5]){
                             echo'
                                 <td>
-                                    <a class="btn btn-sm btn-info btn-embossed" style="text-decoration: none"data-toggle="modal" data-target="#myModal'.$hasil0[0].'">Terima</a>
-                                    <div class="modal fade" id="myModal'.$hasil0[0].'" role="dialog">
+                                    <a><span class="glyphicon glyphicon-plus btn btn-xs btn-info btn-embossed" data-toggle="modal" data-target="#myModal"></span></a>
+                                    <div class="modal fade" id="myModal" role="dialog">
                                       <div class="modal-dialog">
 
                                         <!-- Modal content-->
                                         <div class="modal-content">
                                           <div class="modal-header">
                                             <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                            <h4 class="modal-title">Penerimaan Beasiswa</h4>
+                                            <h4 class="modal-title">Modal Header</h4>
                                           </div>
                                           <div class="modal-body">
 
@@ -171,8 +169,7 @@ $rowBsw = mysqli_fetch_array($bswExc);
                                             <div class="form-group">
                                             <label class="control-label col-sm-4" for="kd_daftar">Kode Daftar :</label>
                                             <div class="col-sm-6">
-                                              <p>'.$hasil0[0].'</p>
-                                              <input type="hidden" class="form-control" id="kd_daftar" name="kd_daftar" value="'.$hasil0[0].'" >
+                                              <input type="text" class="form-control" id="kd_daftar" name="kd_daftar" value="'.$hasil0[0].' disabled">
                                             </div>
                                             </div>
                                             <div class="form-group">
