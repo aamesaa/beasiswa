b5b98a7783069b54eceb086e74a44dfbaa6d0e9f
Charoline Septa <charoline.septa@si.ukdw.ac.id>
2017-05-01 12:29:24 +0700

    Change link

--- user_admin/export.php
+++ user_admin/export.php
@@ -8,5 +8,5 @@ header("Content-type: application/vnd-ms-excel");
 header("Content-Disposition: attachment; filename=beasiswa-export.xls");
 
 // Tambahkan table
-include 'data.php';
+include 'detail_pendaftar.php';
 ?>
