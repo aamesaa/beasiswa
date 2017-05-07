b9a325edf2220b387942c2b889364d43dce4497b
Charoline Septa <charoline.septa@si.ukdw.ac.id>
2017-05-07 19:40:55 +0700

    change data export

--- user_admin/data.php
+++ user_admin/data.php
@@ -7,12 +7,18 @@ Include("../function/koneksi.php");
 <table border="1">
 	<tr>
 		<th>NO.</th>
-		<th>NIM</th>
+		<th>Kode Daftar</th>
+		<th>NIM.</th>
+		<th>Nama</th>
+		<th>Tanggal Daftar</th>
+		<th>Nominal Pengajuan</th>
+		<th>Nominal Disetujui</th>
 	</tr>
 	<?php
 
 	//query menampilkan data
-    $query="SELECT * FROM pendaftaran where kd_bsw='".$_GET['kd_bsw']."' and nominal_disetujui is not null ";
+  $query="SELECT kd_daftar, nim, nama_mhs,tgl_daftar, nominal_pengajuan, nominal_disetujui from pendaftaran p NATURAL JOIN mahasiswa m  where kd_bsw='".$_GET['kd_bsw']."' and nominal_disetujui is not null";
+
 	$sql = mysqli_query($koneksi,$query);
 
 	$no = 1;
@@ -20,7 +26,12 @@ Include("../function/koneksi.php");
 		echo '
 		<tr>
 			<td>'.$no.'</td>
+			<td>'.$data['kd_daftar'].'</td>
 			<td>'.$data['nim'].'</td>
+			<td>'.$data['nama_mhs'].'</td>
+			<td>'.$data['tgl_daftar'].'</td>
+			<td>'.$data['nominal_pengajuan'].'</td>
+			<td>'.$data['nominal_disetujui'].'</td>
 		</tr>
 		';
 		$no++;
