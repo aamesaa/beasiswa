4254942dbcb22cda47822a1c2f2b68d2a6159e18
aamesaa <azhalia.amesa@si.ukdw.ac.id>
2017-05-14 23:44:02 +0700

    Coba upload-download

--- create_daftar_beasiswa.php
+++ create_daftar_beasiswa.php
@@ -6,7 +6,7 @@
  * Time: 11:54 AM
  */
 
-
+/*
 Include("function/koneksi.php");
 include ("function/login.php");
 
@@ -16,8 +16,22 @@ $kd_bsw=$_POST['kd_bsw'];
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 if (1==1)
+    */
 
 
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="daftar_beasiswa.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<a href='download.php?file=baru.pdf'>download</a>
+<a href='uploads/baru.pdf' target="_blank">downlod</a>
+</body>
+</html>
