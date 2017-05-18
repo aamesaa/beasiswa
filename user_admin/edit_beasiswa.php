dfc1cddc42c1ded2a6ecf1116738ead5720cc342
Charoline Septa <charoline.septa@si.ukdw.ac.id>
2017-05-18 15:10:24 +0700

    Fix Edit Bsw

--- user_admin/edit_beasiswa.php
+++ user_admin/edit_beasiswa.php
@@ -2,13 +2,14 @@
 include("../function/koneksi.php");
 $kd_bsw=$_POST['kd_bsw'];
 $nama_bsw=$_POST['namabsw'];
+$semester=$_POST['semester'];
 $thn_ajar=$_POST['thn_ajar'];
 $tgl_mulai=$_POST['mulai'];
 $tgl_selesai=$_POST['tutup'];
 $tampil=$_POST['tampil'];
 $ket=$_POST['ket'];
 $kuota=$_POST['kuota'];
-$sql="UPDATE  beasiswa SET nama_bsw ='$nama_bsw', thn_ajar = '$thn_ajar', tgl_buka = '$tgl_mulai',
+$sql="UPDATE  beasiswa SET nama_bsw ='$nama_bsw', semt = '$semester' ,thn_ajar = '$thn_ajar', tgl_buka = '$tgl_mulai',
       tgl_tutup = '$tgl_selesai', kuota = '$kuota', isTampil = '$tampil', Keterangan = '$ket'
       WHERE kd_bsw = '$kd_bsw' ";
 $hasil = mysqli_query($koneksi,$sql);
